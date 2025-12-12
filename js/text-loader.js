17b5af7fc7d0604f7888adbd0ee0c65d59837a1b
yabe54053 <97826644+yabe54053@users.noreply.github.com>
2025-12-12 11:40:16 +0900

    Add files via upload

--- js/text-loader.js
+++ js/text-loader.js
@@ -3,16 +3,16 @@ document.addEventListener("DOMContentLoaded", () => {
   { id: "about-text", file: "./texts/common/about.txt" },
   { id: "contact-text", file: "./texts/common/contact.txt" },
   { id: "donation-text", file: "./texts/common/donation.txt" },
-  { id: "donation-detail-text", file: "./texts/common/donation-detail.txt" },
+  { id: "donation-detail-text", file: "../texts/common/donation-detail.txt" },
   { id: "event-text", file: "./texts/event/event.txt" },
   { id: "event-detail-text", file: "./texts/event/event-detail.txt" },
   { id: "program-text", file: "./texts/event/program.txt" },
   { id: "access-text", file: "./texts/event/access.txt" },
   { id: "ticket-text", file: "./texts/event/ticket.txt" },
-  { id: "past-event-text", file: "./texts/event/past-event.txt" },
+  { id: "past-event-text", file: "../texts/event/past-event.txt" },
   { id: "head-news-text", file: "./texts/news/head-news.txt" },
-  { id: "past-news-text1", file: "./texts/news/past-news1.txt" },
-  { id: "past-news-text2", file: "./texts/news/past-news2.txt" },
+  { id: "past-news-text1", file: "../texts/news/past-news1.txt" },
+  { id: "past-news-text2", file: "../texts/news/past-news2.txt" },
     // 他にもあればここに追加してOK！program-text
   ];
 

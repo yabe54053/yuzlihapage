38979351854ee08be79d8b4811f066c31c8614f0
yabe54053 <97826644+yabe54053@users.noreply.github.com>
2025-12-12 11:44:39 +0900

    Add files via upload

--- js/text-loader.js
+++ js/text-loader.js
@@ -11,8 +11,8 @@ document.addEventListener("DOMContentLoaded", () => {
   { id: "ticket-text", file: "./texts/event/ticket.txt" },
   { id: "past-event-text", file: "../texts/event/past-event.txt" },
   { id: "head-news-text", file: "./texts/news/head-news.txt" },
-  { id: "past-news-text1", file: "../texts/news/past-news1.txt" },
-  { id: "past-news-text2", file: "../texts/news/past-news2.txt" },
+  { id: "past-news-text1", file: ".../texts/news/past-news1.txt" },
+  { id: "past-news-text2", file: ".../texts/news/past-news2.txt" },
     // 他にもあればここに追加してOK！program-text
   ];
 
